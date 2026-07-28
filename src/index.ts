cdad7e8b1cade95232d72e5ec088eefedb41dd31
Tomáš Hobza <thobza@cloudflare.com>
2026-07-28 02:25:50 +0100

    intentional bug

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export function handleRequest(request: Request): Response {
-  const name = new URL(request.url).searchParams.get('name') ?? 'world';
+  const name = new URL(request.url).searchParams.get('param') ?? 'world';
 
   return new Response(`Hello, ${name}!`, {
     headers: { 'content-type': 'text/plain; charset=utf-8' },
