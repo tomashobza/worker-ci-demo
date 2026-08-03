94c44c52a3a682b2e0ac0ac892c99a22e3eb4425
Tomas Hobza <thobza@cloudflare.com>
2026-08-03 23:57:38 +0100

    fix: read name query param instead of param

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export function handleRequest(request: Request): Response {
-  const name = new URL(request.url).searchParams.get('param') ?? 'world';
+  const name = new URL(request.url).searchParams.get('name') ?? 'world';
 
   return new Response(`Hello, ${name}!`, {
     headers: { 'content-type': 'text/plain; charset=utf-8' },
